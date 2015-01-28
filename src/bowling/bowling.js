f8e4372f57c363f788b1fc995187cc1dabd5d5f4
Mark Pearce <mark.pearce@redspace.com>
2015-01-28 09:52:00 -0400

    Test 3 - input checking

--- src/bowling/bowling.js
+++ src/bowling/bowling.js
@@ -1,11 +1,19 @@
 ERROR = -1;
+SHOTS_PER_FRAME = 3;
 
 Bowling = {
 
   score: function(frames) {
+    var perFrame = SHOTS_PER_FRAME;
+    if(typeof frames.length === 'undefined')
+      return ERROR;
     if(frames.length > 10)
       return ERROR;
 
+    for(var i = 0; i < frames.length; i++) {
+      if(typeof frames[i].length === 'undefined') return ERROR;
+      if(frames[i].length > perFrame) return ERROR;
+    }
 
     return 0;
 
--- src/bowling/bowling.spec.js
+++ src/bowling/bowling.spec.js
@@ -13,12 +13,22 @@ describe('bowling', function () {
     expect(isNumber(b.score([]))).toBe(true);
   });
 
+  it('should return error if input is not array', function(){
+    expect(b.score(1)).toBe(-1);
+  });
+
+
   it('should return error (-1) if array has more than ten values', function(){
     var score = b.score([1,1,1,1,1,1,1,1,1,1,1]);
     expect(score).toBe(-1);
   });
 
 
+  it('should return error if any element in the input has more than 3 values', function(){
+    var score = b.score([ [1,1,1], [1,1,1,1]]);
+    expect(score).toBe(-1);
+
+  });
 
 
 });
